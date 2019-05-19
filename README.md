855e306140583e35ccfa39e534112ed6c11757e4
voduylinh31191 <voduylinh31191@gmail.com>
2019-05-19 15:06:31 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dbqh8r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dbqh8r)
---
+++ app/cdk-drag-drop-axis-lock-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Drag&Drop position locking
+ */
+@Component({
+  selector: 'cdk-drag-drop-axis-lock-example',
+  templateUrl: 'cdk-drag-drop-axis-lock-example.html',
+  styleUrls: ['cdk-drag-drop-axis-lock-example.css'],
+})
+export class CdkDragDropAxisLockExample {}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
